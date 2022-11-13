a4245423bb27d6b5a7a4432f4427e1d001103ff0
Hexlir <el.public@programmer.net>
2022-11-13 20:35:13 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'Hexlir/master'.
    
    Changes to be committed:
    	new file:   PEMDAS/stringofarray/tugas1.cpp
    	deleted:    Toki/bajukonser
    	deleted:    Toki/bajukonser.cpp
    	deleted:    Toki/dengklekdeadliner
    	deleted:    Toki/dengklekdeadliner.cpp
    	deleted:    Toki/kisahdalamsumur.cpp

---
+++ PEMDAS/stringofarray/tugas1.cpp
@@ -0,0 +1,44 @@
+// BRIEF
+
+// Lakukan percobaan untuk menginputkan string dari keyboard dengan menggunakan : 
+// scanf(), gets() dan fgets(). 
+// Analisislah dan berikan kesimpulan untuk setiap fungsi tsb!
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "PERCOBAAN INPUT STRING DENGAN FUNGSI SCANF(), GETS() DAN FGETS()" << endl;
+    cout << "================================================================" << endl;
+    cout << "Masukkan string dengan fungsi scanf() : ";
+
+    char str[100];
+    printf("Masukkan string: ");
+    scanf("%s", str);
+    printf("String yang dimasukkan: %s", str);
+    cout << endl;
+
+    cout << "Masukkan string dengan fungsi gets() : ";
+    char str1[100];
+    gets(str1);
+    printf("String yang dimasukkan: %s", str1);
+    cout << endl;
+
+    cout << "Masukkan string dengan fungsi fgets() : ";
+    char str2[100];
+    fgets(str2, sizeof(str2), stdin);
+    printf("String yang dimasukkan: %s", str2);
+    cout << endl;
+
+    cout << "================================================================" << endl;
+    cout << "KESIMPULAN" << endl;
+    cout << "================================================================" << endl;
+    cout << "1. Fungsi scanf() hanya dapat menginputkan string yang berupa satu kata saja" << endl;
+    cout << "2. Fungsi gets() dapat menginputkan string yang berupa satu kata atau lebih" << endl;
+    cout << "3. Fungsi fgets() dapat menginputkan string yang berupa satu kata atau lebih" << endl;
+    cout << "================================================================" << endl;
+
+    return 0;
+}
